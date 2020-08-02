74938e672ed0a8b5b4b55edd3f7d490348f9d96a
iliakryt <iliapriyomko@mail.ru>
2020-08-02 21:47:04 +0600

    Fix of encoding

--- ProductsViewGemsdevTests/GemsDevTests.cs
+++ ProductsViewGemsdevTests/GemsDevTests.cs
@@ -7,7 +7,7 @@ namespace ProductsViewGemsdevTests
     public class GemsDevTests
     {
         private IWebDriver driver;
-        private readonly By _productButton = By.XPath("//header//li[@id = 'menu-item-45']//a[text() = 'Продукты']");      
+        private readonly By _productButton = By.XPath("//header//li[@id = 'menu-item-45']//a[text() = 'РџСЂРѕРґСѓРєС‚С‹']");      
         private readonly By _referenceGosSystem = By.XPath("//section[@class='gos-system bg_circle']/descendant::a[@href = 'https://xn--c1aaceme9acfqh.xn--p1ai/']");
         
         [SetUp]
@@ -23,28 +23,28 @@ namespace ProductsViewGemsdevTests
         public void ViewGeoMetaTest()
         {
             var checkElement = new CheckElements(driver);
-            Assert.IsTrue(checkElement.CheckSectionViewGeoMeta(), "Раздел \"GeoMeta\" не найдет");
+            Assert.IsTrue(checkElement.CheckSectionViewGeoMeta(), "Р Р°Р·РґРµР» \"GeoMeta\" РЅРµ РЅР°Р№РґРµС‚");
         }
 
         [Test]
         public void ViewGosSystemTest()
         {
             var checkElement = new CheckElements(driver);
-            Assert.IsTrue(checkElement.CheckSectionViewGosSystem(), "Раздел \"Государственная система обеспечения градостроительной деятельности\" не найдет");
+            Assert.IsTrue(checkElement.CheckSectionViewGosSystem(), "Р Р°Р·РґРµР» \"Р“РѕСЃСѓРґР°СЂСЃС‚РІРµРЅРЅР°СЏ СЃРёСЃС‚РµРјР° РѕР±РµСЃРїРµС‡РµРЅРёСЏ РіСЂР°РґРѕСЃС‚СЂРѕРёС‚РµР»СЊРЅРѕР№ РґРµСЏС‚РµР»СЊРЅРѕСЃС‚Рё\" РЅРµ РЅР°Р№РґРµС‚");
         }
 
         [Test]
         public void ViewUrbanAnalyticsTest()
         {
             var checkElement = new CheckElements(driver);
-            Assert.IsTrue(checkElement.CheckSectionViewUrbanAnalytics(), "Раздел \"Городская аналитика\" не найдет");
+            Assert.IsTrue(checkElement.CheckSectionViewUrbanAnalytics(), "Р Р°Р·РґРµР» \"Р“РѕСЂРѕРґСЃРєР°СЏ Р°РЅР°Р»РёС‚РёРєР°\" РЅРµ РЅР°Р№РґРµС‚");
         }
 
         [Test]
         public void ViewOtherProductsTest()
         {
             var checkElement = new CheckElements(driver);
-            Assert.IsTrue(checkElement.CheckSectionViewOtherProducts(), "Раздел \"Другие наши продукты\" не найдет");
+            Assert.IsTrue(checkElement.CheckSectionViewOtherProducts(), "Р Р°Р·РґРµР» \"Р”СЂСѓРіРёРµ РЅР°С€Рё РїСЂРѕРґСѓРєС‚С‹\" РЅРµ РЅР°Р№РґРµС‚");
         }
 
         [Test]
@@ -52,7 +52,7 @@ namespace ProductsViewGemsdevTests
         {
             Thread.Sleep(500);
             var actualReferenceGosSystem = driver.FindElement(_referenceGosSystem);
-            Assert.IsNotNull(actualReferenceGosSystem, "Ссылка не найдена");
+            Assert.IsNotNull(actualReferenceGosSystem, "РЎСЃС‹Р»РєР° РЅРµ РЅР°Р№РґРµРЅР°");
         }
 
         [TearDown]
